c0d7ee3c8b6deab92002a39442e7cb91b33da3df
ImMin5 <mino@megazone.com>
2023-07-16 23:55:54 +0900

    build: add packag pymongo

--- src/atable_lab/wine_info_app/controller/wine_info_controller.py
+++ src/atable_lab/wine_info_app/controller/wine_info_controller.py
@@ -1,7 +1,4 @@
 from fastapi.encoders import jsonable_encoder
-from pymongo import MongoClient
-from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.server_api import ServerApi
 from atable_lab.wine_info_app.controller import BaseController
 from atable_lab.wine_info_app.model.wine_info_model import WineInfo
 
